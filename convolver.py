4bbd6e8334eae4ea2982b4ff0813799f20f8e2d8
Clinton McKay <ziesunkownemail@clueless.duh>
2014-03-10 09:36:03 -0600

    The derive functions will now pass over all of the cells in teh array

--- convolver.py
+++ convolver.py
@@ -4,35 +4,19 @@ import matplotlib.pyplot as plt
 import math as m
 import scipy.signal as sig
 
-    #[[coeffs],[index offset]]
-###############################################################
-    #first derivative h^2(2h)
-##    cf12=[[-1,1],[-1,1]]
-##    lcf12=[[1,-4,3],[-2,-1,0]]
-##    rcf12=[[-3,4,-1],[0,1,2]]
-##    #first derivative h^4(12h)
-##    cf14=[[1,-8,8,-1],[-2,-1,1,2]]
-##    lcf14=[[3,-16,36,-48,25],[-4,-3,-2,-1,0]]
-##    rcf14=[[-25,48,-36,16,-3],[0,1,2,3,4]]
-##    #first derivative h^6(60h)
-##    cf16=[[-1,9,-45,45,-9,1],[-3,-2,-1,1,2,3]]
-##    lcf16=[[10,-72,225,-400,450,-360,147],[-6,-5,-4,-3,-2,-1,0]]
-##    rcf16=[[-147,360,-450,400,-225,72,-10],[0,1,2,3,4,5,6]]
-####################################################################
-##    #Second Derivative h^2(h^2)
-##    cf22=[[1,-2,1],[-1,0,1]]
-##    lcf22=[[-1,4,-5,2],[-3,-2,-1,0]]
-##    rcf22=[[2,-5,4,-1],[0,1,2,3]]
-##    #second derivative h^4 (12h^2)
-##    cf24=[[-1,16,-30,16,-1],[-2,-1,0,1,2]]
-##    lcf24=[[-10,61,-156,214,-154,45],[-5,-4,-3,-2,-1,0]]
-##    rcf24=[[45,-154,214,-156,61,-10],[0,1,2,3,4,5]]
-##    #second derivative h^6 (180h^2)
-##    cf26=[[2,-27,270,-490,270,-27,2],[-3,-2,-1,0,1,2,3]]
-##    lcf26=[[-126,1019,-3618,7380,-9490,7911,-4014,938],[-7,-6,-5,-4,-3,-2,-1,0]]
-##    rcf26=[[938,-4014,7911,-9490,7380,-3618,1019,-126],[0,1,2,3,4,5,6,7]]
-################################################################################
-def derive(y,cf,lcf,rcf,h):
+def derive_dot(y,cf,lcf,rcf,h):
+    """ Calculates the derivative using the dot product.
+        :param y: The y values of the of the discretized function with step a size of h.
+        :param cf: The center function coefficients and indexes. 
+            Expects a 2xN array. Where row one contains the coefficients and row two contains the 
+            the relative indexes to access y. 
+        :param lcf: The left side function. Just as with cf expects two rows. the first containing the 
+            coefficients and the second containing the indexes. 
+        :param rcf: The right side function. Just as with cf expects two rows. 
+        :param h: The h value as an array. The first index is the h size, the second is the 
+            coefficient to multiply h by. The third is the power to raise h. 
+            e.x. [h, c, b] => c * h^b
+    """
     cf=np.array(cf).astype(float)
     lcf=np.array(lcf).astype(float)
     rcf=np.array(rcf).astype(float)
@@ -43,8 +27,8 @@ def derive(y,cf,lcf,rcf,h):
 
  
     out=[]
+    # Right differential 
     for i in range(0, np.abs(starti)):
-        #print i
         indexes = np.array(rcf[1]).astype(int) + i
         y_s = np.array(y[indexes])
         dydx=1/(h[1]*(h[0]**h[2]))*np.dot(y_s,rcf[0])
@@ -58,9 +42,7 @@ def derive(y,cf,lcf,rcf,h):
         out.append(dydx)
     
     # Left differential
-    #print len(y)
     for i in range(len(y) - stopi, len(y)):
-        #print i
         indexes = np.array(lcf[1]).astype(int) + i
         y_s = y[indexes]
         dydx=1/(h[1]*(h[0]**h[2]))*np.dot(y_s,lcf[0])
@@ -68,24 +50,39 @@ def derive(y,cf,lcf,rcf,h):
     
     return out
 
-def derive2(y,cf,lcf,rcf,h):
+def derive_conv(y,cf,lcf,rcf,h):
+    """ Calculates the derivative using the scipy.signal.convolve.
+        :param y: The y values of the of the discretized function with step a size of h.
+        :param cf: The center function coefficients and indexes. 
+            Expects a 2xN array. Where row one contains the coefficients and row two contains the 
+            the relative indexes to access y. 
+        :param lcf: The left side function. Just as with cf expects two rows. the first containing the 
+            coefficients and the second containing the indexes. 
+        :param rcf: The right side function. Just as with cf expects two rows. 
+        :param h: The h value as an array. The first index is the h size, the second is the 
+            coefficient to multiply h by. The third is the power to raise h. 
+            e.x. [h, c, b] => c * h^b
+    """
     cf=np.array(cf).astype(float)
     lcf=np.array(lcf).astype(float)
     rcf=np.array(rcf).astype(float)
     h=np.array(h).astype(float)
+
+    # Center differential
+    starti=int(cf[1][0])
+    stopi=int(cf[1][-1])
     
     out=[]
 
     # Right-hand differential
-    indexes = np.array(rcf[1]).astype(int)
-    y_s = np.array(y[indexes])
-    conv=np.array(sig.convolve(y_s,rcf[0],'valid'))
-    dydx=(1/(h[1]*(h[0]**h[2])))*conv
-    out.append(-1*np.sum(dydx))
-    
+    for i in range(0, np.abs(starti)):
+        indexes = np.array(rcf[1]).astype(int) + i
+        y_s = np.array(y[indexes])
+        conv=np.array(sig.convolve(y_s,rcf[0],'valid'))
+        dydx=(1/(h[1]*(h[0]**h[2])))*conv
+        out.append(-1*np.sum(dydx))
+   
     # Center differential
-    starti=int(cf[1][0])
-    stopi=int(cf[1][-1])
     for i in range(abs(starti),len(y)-stopi,1):
         indexes = np.array(cf[1]).astype(int) + i
         y_s = np.array(y[indexes])
@@ -94,11 +91,12 @@ def derive2(y,cf,lcf,rcf,h):
         out.append(-1*np.sum(dydx))
     
     # Left-hand differential 
-    indexes = np.array(lcf[1]).astype(int)+(len(y)-1)
-    y_s = y[indexes]
-    conv=sig.convolve(y_s,lcf[0],'valid')
-    dydx=(1/(h[1]*(h[0]**h[2])))*conv
-    out.append(-1*np.sum(dydx))
+    for i in range(len(y) - stopi, len(y)):
+        indexes = np.array(lcf[1]).astype(int) + i
+        y_s = y[indexes]
+        conv=sig.convolve(y_s,lcf[0],'valid')
+        dydx=(1/(h[1]*(h[0]**h[2])))*conv
+        out.append(-1*np.sum(dydx))
     
     return out
 
